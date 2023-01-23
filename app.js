180ea014f8ea497d8f0d13e19fc40cd3fa8df9ca
Javier Alonso Valderrabano Jaimes <valderrabano_jaimes@hotmail.com>
2023-01-23 16:21:13 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+let string = 'Javier';
+var edad = 25;
+const boolean = true;
+let num1 = 12;
+let num2 = -64;
+
+console.log(`Hola mi nombre es ${string} y tengo ${edad}`);
+console.log((num2*=num2)*(num1 + num2)/(num1*= num2));
+if(num1 >= num2)
+console.log(`El numero ${num1} es mayor que ${num2}`);
