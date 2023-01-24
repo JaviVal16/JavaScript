ff5256c5b57af2ef1d499a698668a2bd1cc5d788
Javier Alonso Valderrabano Jaimes <valderrabano_jaimes@hotmail.com>
2023-01-23 19:42:39 -0600

    Update app.js
    Actividad de funciones

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let string = 'Javier';
+/* let string = 'Javier';
 var edad = 25;
 const boolean = true;
 let num1 = 12;
@@ -8,3 +8,47 @@ console.log(`Hola mi nombre es ${string} y tengo ${edad}`);
 console.log((num2*=num2)*(num1 + num2)/(num1*= num2));
 if(num1 >= num2)
 console.log(`El numero ${num1} es mayor que ${num2}`);
+ */
+
+let num1 = 12, num2 = 6, num3 = 24;
+
+let suma = () => num1 + num2 + num3;
+
+console.log(suma());
+
+let compare = () => {
+    if (num1 >= num2 && num1 <= num3) {
+        return `El numero ${num1} es mayor que ${num2} y menor que ${num3}`;
+    } else {
+        return `La coondcion no se cumple`
+    }
+}
+
+console.log(compare());
+
+let caricatura = {
+    nombre: 'Lonney Tunes',
+    capitulo: 'Do not Give Up The Sheep',
+    año: 1953,
+    personajes: {
+        principal: 'Coyote',
+        secundario: 'Ralph'
+    },
+    frases: ['¡Oh, buenos días Ralph!', 'Otro día... otro dolar', 'Hola Fred']
+}
+
+let { nombre, personajes, frases } = caricatura;
+console.log(nombre, personajes.principal, frases[0]);
+
+
+let elArray = ["it's me. Mario", ['Que el profe Alan se rasque la nariz y un ojo al mismo tiempo la proxima clase si leyó esto', 
+                                    'expecto patronuuuuum'], 
+                24, 23, ];
+
+const funArry = () => {
+    return elArray;
+}
+
+const [frase, [otraFrase], numero] = funArry();
+
+console.log(frase, [otraFrase], numero);
