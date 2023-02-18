65c9d5bfaf1b89c84a60797cef67498645e128e1
Javier Alonso Valderrabano Jaimes <valderrabano_jaimes@hotmail.com>
2023-02-18 13:01:02 -0600

    Titulos

--- apiLocal.js
+++ apiLocal.js
@@ -9,15 +9,21 @@ apiBtnLocal.addEventListener('click', () => {
 const apiCall2 = () => {
     charactersDivLocal.innerHTML = '';
     fetch(localGet)
-    .then(data => data.json())
-    .then(info => info.forEach(personajes => {
-        charactersDivLocal.innerHTML += 
-        `
+        .then(data => data.json())
+        .then(info => info.forEach(personajes => {
+            charactersDivLocal.innerHTML +=
+                `
         <div class="card">
             <h2>${personajes.nombre}</h2>
+            <div id="container">
             <img src="${personajes.img}" alt="${personajes.nommbre}">
+                <div class="box-text">
+                    <h3>${personajes.nombre}</h3>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                </div>
+            </div>
             <p>${personajes.descripcion}</p>
         </div>
         `
-    }))
+        }))
 };
